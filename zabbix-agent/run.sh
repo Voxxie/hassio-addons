5e3dff08ea2bea92177f7f72e8a59caf6a23d188
Voxxie <hansvos@ingestel.nl>
2020-09-28 14:46:26 +0200

    0.1.6

--- zabbix-agent/run.sh
+++ zabbix-agent/run.sh
@@ -6,8 +6,12 @@ CUSTOM_CFG_PATH=/share/zabbix-agent
 
 SERVER=$(jq --raw-output ".server" $CONFIG_PATH)
 HOSTNAME=$(jq --raw-output ".hostname" $CONFIG_PATH)
+PSK=$(jq --raw-output ".psk" $CONFIG_PATH)
 
-echo "DEPLOYING ZABBIX AGENT $PSK"
+echo "------------DEPLOYING ZABBIX AGENT-----------"
+echo "HOSTNAME: $HOSTNAME"
+echo "PSK: $PSK"
+echo "------------DEPLOYING ZABBIX AGENT-----------"
 
 echo "
 Server=$SERVER
@@ -21,9 +25,7 @@ LogType=console
 " > /etc/zabbix/zabbix_agentd.conf
 
 
-echo "
-$PSK
-" > /etc/zabbix/zabbix_agentd.psk
+echo "$PSK" > /etc/zabbix/zabbix_agentd.psk
 
 
 

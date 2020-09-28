872162aaa8ed431e22984f5bdfa101122b92a036
Voxxie <hansvos@ingestel.nl>
2020-09-28 14:30:33 +0200

    v0.1.2

--- zabbix-agent/run.sh
+++ zabbix-agent/run.sh
@@ -7,10 +7,6 @@ CUSTOM_CFG_PATH=/share/zabbix-agent
 SERVER=$(jq --raw-output ".server" $CONFIG_PATH)
 HOSTNAME=$(jq --raw-output ".hostname" $CONFIG_PATH)
 
-if [ ! -d "$CUSTOM_CFG_PATH" ] ; then
-  mkdir -p "$CUSTOM_CFG_PATH"
-fi
-
 echo "
 Server=$SERVER
 ServerActive=$SERVER
@@ -20,15 +16,12 @@ TLSAccept=psk
 TLSPSKIdentity=$HOSTNAME
 TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
 LogType=console
-Include=${CUSTOM_CFG_PATH}/*.conf
 " > /etc/zabbix/zabbix_agentd.conf
 
 
 echo "
 $PSK
 " > /etc/zabbix/zabbix_agentd.psk
-
-
 
 
 
